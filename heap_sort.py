b2947a60824962619bdd3cfb73c38c3ec12e86c5
svarogjk <svarogjk1989@gmail.com>
2017-07-20 17:45:36 +0300

    heap_queue_completed

--- heap_sort.py
+++ heap_sort.py
@@ -8,7 +8,6 @@ class Heap():
         self.heap_height = log2(len(self.array))
 
 
-
     def parent(self, i: int)->int:
         return i // 2
 
@@ -58,16 +57,50 @@ class Heap():
             print(self.array)
 
 
+class HeapQueue(Heap):
+
+
+    def heap_maximum(self):
+        return self.array[0]
+
+
+    def heap_extract_max(self):
+        if self.array == []:
+            raise ValueError("empty array!!!")
+        self.array[0], self.array[len(self.array) - 1] = self.array[len(self.array) - 1], self.array[0]
+        _max = self.array.pop()
+        self.max_heapify(self.array, 0)
+        return _max
+
+
+    def heap_increase_key(self):
+        i = len(self.array) - 1
+        while i > 0 and self.array[self.parent(i)] < self.array[i]:
+            self.array[i], self.array[self.parent(i)] = self.array[self.parent(i)], self.array[i]
+            i = self.parent(i)
+
+
+    def max_heap_insert(self, key):
+        self.array.append(key)
+        self.heap_increase_key()
+
+
 if __name__ == '__main__':
     from unittest import TestCase, main
 
     # array = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
-    array = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
+    array = [15, 13, 9, 5, 12, 8, 7, 4, 0, 6, 2, 1]
 
-    h = Heap(array)
+    h = HeapQueue(array)
     # h.max_heapify(h.array, 1)
-    h.heap_sort()
+    # h.heap_sort()
+    h.build_max_heap()
     print(h.array)
+
+    insert_array = [20, 2, 25, 3, 26, 5]
+    for e in insert_array:
+        h.max_heap_insert(e)
+        print(h.array)
 
 
     # class SimpleTest(TestCase):
@@ -76,5 +109,3 @@ if __name__ == '__main__':
     #         self.assertEqual(h.array, sorted(A))
     #
     # main()
-
-
